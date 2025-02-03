c7591178123363be59b53256a8f354307a626466
Diego Andrés Ramírez Mejía <diego.rm.dev@gmail.com>
2025-02-02 21:52:06 -0500

    feat(5): fix details in sidebar and footer

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -78,7 +78,7 @@ const config: Config = {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Documentation',
         },
         {
           href: 'https://github.com/facebook/docusaurus',
@@ -126,7 +126,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Parfinanciero, Inc. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
